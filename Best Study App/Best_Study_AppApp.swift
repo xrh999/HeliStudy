e71959b60aebafe7ac390a67ff4c1008147a8072
xrh999 <xrh999@gmail.com>
2025-08-04 13:27:14 +0800

    Initial Commit

---
+++ Best Study App/Best_Study_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Best_Study_AppApp.swift
+//  Best Study App
+//
+//  Created by Huang XR on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Best_Study_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
