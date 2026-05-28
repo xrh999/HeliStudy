306c32fd1a6fe3548e33376af034dcf171c8e214
xrh999 <xrh999@gmail.com>
2026-05-28 19:33:58 +0800

    Initial Commit

---
+++ HeliSpaced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeliSpaced
+//
+//  Created by Huang XR on 28/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeliSpaced/HeliSpacedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeliSpacedApp.swift
+//  HeliSpaced
+//
+//  Created by Huang XR on 28/5/26.
+//
+
+import SwiftUI
+
+@main
+struct HeliSpacedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
