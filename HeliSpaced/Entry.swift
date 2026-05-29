c4794f24666c242e9b6122ce3a57606b1a94e6fd
xrh999 <xrh999@gmail.com>
2026-05-29 13:15:10 +0800

    ACTUALLY create entry class.

--- HeliSpaced/Entry.swift
+++ HeliSpaced/Entry.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftData
+var fib = [1, 2, 3]
+
+@Model
+class Entry: Equatable {
+    var name: String
+    var dateCreated: Date
+    var desc: String?
+    var repCnt: Int
+    var nextDate: Date
+
+    func reviewed() {
+        if (repCnt >= fib.count) {
+            while (fib.count <= repCnt) {
+                fib.append(fib[fib.count - 1] + fib[fib.count - 2]);
+            }
+        }
+        nextDate = Calendar.current.date(byAdding: .day, value: fib[repCnt], to: nextDate)!
+        repCnt += 1
+    }
+    
+    init (name: String, dateCreated: Date = Date(), desc: String? = nil, repCnt: Int = 0, nextDate: Date = Date()) {
+        self.name = name
+        self.dateCreated = dateCreated
+        self.desc = desc
+        self.repCnt = repCnt
+        self.nextDate = nextDate
+    }
+}
