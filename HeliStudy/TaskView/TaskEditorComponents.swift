890fbbf00e5735cb7d73038481b758d3e2d3e63e
xrh999 <xrh999@gmail.com>
2026-06-07 22:51:00 +0800

    Refactor TaskEditorView to be broken up into structs in TaskEditorComponents for faster type checking.

---
+++ HeliStudy/TaskView/TaskEditorComponents.swift
@@ -0,0 +1,129 @@
+//
+//  TaskEditorComponents.swift
+//  HeliStudy
+//
+//  Created by Huang XR on 7/6/26.
+//
+
+import SwiftUI
+
+struct TaskPicker: View {
+    @Binding var draft: DraftTask
+    var body: some View {
+        Picker (selection: $draft.taskType) {
+            Text("Spaced").tag(TaskType.sr)
+            Text("Repeated").tag(TaskType.repeated)
+            Text("Normal").tag(TaskType.normal)
+        } label: {
+            Text("")
+        }
+        .pickerStyle(.segmented)
+        .frame(width: 367)
+        .padding(6)
+    }
+}
+
+struct MandatoryTaskItems: View {
+    @Binding var draft: DraftTask
+    @FocusState private var isKeyboardFocused: Bool
+    var body: some View{
+        Section(header: Text("Task info")) {
+            TextField("Task Name", text: $draft.taskName)
+                .focused($isKeyboardFocused)
+            TextField("Description (optional)", text: $draft.taskDesc)
+                .focused($isKeyboardFocused)
+        }
+    }
+}
+
+struct NewSRTaskView: View {
+    @Binding var draft: DraftTask
+    var body: some View {
+        Section(header: Text("Times repeated")) {
+            Stepper(value: $draft.repCnt, in: 0...Int.max) {
+                Text("\(draft.repCnt)")
+            }
+        }
+    }
+}
+
+struct NewRepeatedTaskView: View {
+    let dateRange = Date.now...Date.distantFuture
+    @Binding var draft: DraftTask
+    @FocusState private var isKeyboardFocused: Bool
+    var body: some View {
+        
+        Section(header: Text("Repeat")) {
+            Toggle(isOn: $draft.endRepeat) {
+                Text("End repeat")
+            }
+            if (draft.endRepeat) {
+                DatePicker("End date:", selection: $draft.endDate, in: dateRange, displayedComponents: .date)
+            }
+            TextField("Repeat interval (in days)", value: $draft.repeatInterval, format: .number)
+                .keyboardType(.numberPad)
+                .focused($isKeyboardFocused)
+        }
+    }
+}
+
+struct NewNormalTaskView: View {
+    let dateRange = Date.now...Date.distantFuture
+    @Binding var draft: DraftTask
+    var body: some View {
+        Section(header: Text("Schedule")) {
+            DatePicker("Due date", selection: $draft.endDate, in: dateRange, displayedComponents: [.date, .hourAndMinute])
+        }
+    }
+}
+
+struct LeadingToolbarView: View {
+    var mode: EditorMode
+    @Binding var draft: DraftTask
+    @Binding var confirmDeletion: Bool
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        switch mode {
+        case .create:
+            Button {
+                if (!draft.taskName.isEmpty) { confirmDeletion.toggle() }
+                else { dismiss() }
+            } label: {
+                Image(systemName: "trash")
+            }
+            .tint(.red)
+        case .edit:
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "chevron.backward")
+            }
+        }
+    }
+}
+
+struct TrailingToolbarView: View {
+    var mode: EditorMode
+    var onSave: () -> Void
+    @Binding var draft: DraftTask
+    @Binding var showEmptyAlert: Bool
+    @Binding var showEditedAlert: Bool
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        Button {
+            // TODO: Expand check to all fields
+            if (draft.taskName.isEmpty) {
+                showEmptyAlert = true
+            } else {
+                if case .edit = mode {
+                    showEditedAlert.toggle()
+                }
+                onSave()
+            }
+        } label: {
+            Image(systemName: "checkmark")
+        }
+        .buttonStyle(.borderedProminent)
+    }
+}
--- HeliStudy/TaskView/TaskEditorView.swift
+++ HeliStudy/TaskView/TaskEditorView.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct TaskEditorView: View {
     let task: Task?
     var mode: EditorMode
-    let dateRange = Date.now...Date.distantFuture
     @State private var draft: DraftTask
     @State private var confirmDeletion = false
     @State private var showEmptyAlert = false
@@ -32,20 +31,20 @@ struct TaskEditorView: View {
         case .create:
             "Create Task"
         case .edit:
-            "Edit \(draft.taskName)"
+            "Edit Task"
         }
         Group {
             VStack(spacing: 0) {
-                TaskPicker
+                TaskPicker(draft: $draft)
                 Form {
-                    MandatoryTaskItems
+                    MandatoryTaskItems(draft: $draft)
                     switch draft.taskType {
                     case .sr:
-                        NewSRTaskView
+                        NewSRTaskView(draft: $draft)
                     case .repeated:
-                        NewRepeatedTaskView
+                        NewRepeatedTaskView(draft: $draft)
                     case .normal:
-                        NewNormalTaskView
+                        NewNormalTaskView(draft: $draft)
                     }
                 }
                 .onTapGesture {
@@ -83,67 +82,11 @@ struct TaskEditorView: View {
             .alert("Please fill it in", isPresented: $showEmptyAlert) {
                 Button("Ok", role: .cancel) {showEmptyAlert = false}
             }
-           .padding(5)
+            .padding(5)
             .background(
                 Color(uiColor: .systemGroupedBackground)
                     .ignoresSafeArea()
             )
-        }
-    }
-    
-    private var TaskPicker: some View {
-        Picker (selection: $draft.taskType) {
-            Text("Spaced").tag(TaskType.sr)
-            Text("Repeated").tag(TaskType.repeated)
-            Text("Normal").tag(TaskType.normal)
-        } label: {
-            Text("")
-        }
-        .pickerStyle(.segmented)
-        .frame(width: 367)
-        .padding(6)
-    }
-    
-    private var MandatoryTaskItems: some View {
-        Section(header: Text("Task info")) {
-            TextField("Task Name", text: $draft.taskName)
-                .focused($isKeyboardFocused)
-            TextField("Description (optional)", text: $draft.taskDesc)
-                .focused($isKeyboardFocused)
-        }
-    }
-
-    private var NewSRTaskView: some View {
-        Group {
-            Section(header: Text("Times repeated")) {
-                Stepper(value: $draft.repCnt, in: 0...Int.max) {
-                    Text("\(draft.repCnt)")
-                }
-            }
-        }
-    }
-    
-    private var NewRepeatedTaskView: some View {
-        Group {
-            Section(header: Text("Repeat")) {
-                Toggle(isOn: $draft.endRepeat) {
-                    Text("End repeat")
-                }
-                if (draft.endRepeat) {
-                    DatePicker("End date:", selection: $draft.endDate, in: dateRange, displayedComponents: .date)
-                }
-                TextField("Repeat interval (in days)", value: $draft.repeatInterval, format: .number)
-                    .keyboardType(.numberPad)
-                    .focused($isKeyboardFocused)
-            }
-        }
-    }
-    
-    private var NewNormalTaskView: some View {
-        Group {
-            Section(header: Text("Schedule")) {
-                DatePicker("Due date", selection: $draft.endDate, in: dateRange, displayedComponents: [.date, .hourAndMinute])
-            }
         }
     }
     
@@ -165,57 +108,6 @@ struct TaskEditorView: View {
             draft.apply(to: task)
         }
         try? context.save()
-    }
-}
-
-struct LeadingToolbarView: View {
-    var mode: EditorMode
-    @Binding var draft: DraftTask
-    @Binding var confirmDeletion: Bool
-    @Environment(\.dismiss) private var dismiss
-    var body: some View {
-        switch mode {
-        case .create:
-            Button {
-                if (!draft.taskName.isEmpty) { confirmDeletion.toggle() }
-                else { dismiss() }
-            } label: {
-                Image(systemName: "trash")
-            }
-            .tint(.red)
-        case .edit:
-            Button {
-                dismiss()
-            } label: {
-                Image(systemName: "chevron.backward")
-            }
-        }
-    }
-}
-
-struct TrailingToolbarView: View {
-    var mode: EditorMode
-    var onSave: () -> Void
-    @Binding var draft: DraftTask
-    @Binding var showEmptyAlert: Bool
-    @Binding var showEditedAlert: Bool
-    @Environment(\.dismiss) private var dismiss
-    
-    var body: some View {
-        Button {
-            // TODO: Expand check to all fields
-            if (draft.taskName.isEmpty) {
-                showEmptyAlert = true
-            } else {
-                if case .edit = mode {
-                    showEditedAlert.toggle()
-                }
-                onSave()
-            }
-        } label: {
-            Image(systemName: "checkmark")
-        }
-        .buttonStyle(.borderedProminent)
     }
 }
 
